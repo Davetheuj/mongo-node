245e2ee75fdebd8c3e7973a2c064368404ee338a
Davetheuj <Davetheuj@gmail.com>
2023-06-21 15:59:46 -0700

    adjust response status values

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,13 @@ let User = require('../models/user.model');
 
 //get all users
 router.route('/').get((req,res) => {
-    User.find().then(users => res.json(users)).catch(err => res.status(400).json('Error: ' + err));
+    User.find().then(users => res.json(users)).catch(err => res.status(401).json('Error: ' + err));
     
 });
 
 //get one user
 router.route('/:id').get((req,res) => {
-    User.find().then(users => res.json(users)).catch(err => res.status(400).json('Error: ' + err));
+    User.find().then(users => res.json(users)).catch(err => res.status(401).json('Error: ' + err));
     
 });
 
@@ -22,7 +22,7 @@ router.route('/add').post((req,res) => {
     const newUser = new User({username, password, email_address});
 
 
-    newUser.save().then(() => res.json('User has been added')).catch(err => res.status(400).json('Error: ' + err));
+    newUser.save().then(() => res.json('User has been added')).catch(err => res.status(401).json('Error: ' + err));
     
 });
 
@@ -30,7 +30,7 @@ router.route('/add').post((req,res) => {
 router.route('/delete').delete((req,res) =>{
 
   User.findOneAndDelete({username: req.body.username, password: req.body.password}).then(()=> 
-    res.json('User has been deleted')).catch(err => res.status(400).json('Error: ' + err));
+    res.json('User has been deleted')).catch(err => res.status(401).json('Error: ' + err));
     
   
 });
