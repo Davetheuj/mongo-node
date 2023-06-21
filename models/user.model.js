e55952cca31361b5a61194bf087286293a34ba6a
Davetheuj <Davetheuj@gmail.com>
2023-06-21 16:45:55 -0700

    adjust response status values

--- models/user.model.js
+++ models/user.model.js
@@ -16,6 +16,6 @@ const userSchema = new Schema(
     }
 );
 
-const User = mongoose.model('User', userSchema, "users");
+const User = mongoose.model('User', userSchema, "nerds");
 
 module.exports = User;
