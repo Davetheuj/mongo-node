601f19377d361b751eb38ef7b8c0598b01145c10
Davetheuj <Davetheuj@gmail.com>
2023-07-01 18:57:03 -0700

    modify

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 let User = require('../models/user.model');
 
 //get all users
-router.route('/').get((req,res) => {
-    User.find().then(users => res.json(users)).catch(err => res.status(401).json('Error: ' + err));
+// router.route('/').get((req,res) => {
+//     User.find().then(users => res.json(users)).catch(err => res.status(401).json('Error: ' + err));
     
-});
+// });
 
 //get one user
 router.route('/:id').get((req,res) => {
