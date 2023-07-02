29bc5968c177d1701dad34196a20b82cfbb222f7
Davetheuj <Davetheuj@gmail.com>
2023-07-01 19:36:24 -0700

    modify

--- routes/users.js
+++ routes/users.js
@@ -43,7 +43,7 @@ router.route('/delete').delete((req,res) =>{
 router.route('/login').get((req,res) =>{
  
     console.log("running this route");
-    colose.log(req.body);
+    console.log(req.body);
     User.find({username: req.body.username, password: req.body.password}).then(()=> 
       res.json('User has been authenticated')).catch(err => res.status(401).json('Error: ' + err));        
   });
