d9998bdd0f87fd467aee4f8d8bb90b25fffbf442
Davetheuj <Davetheuj@gmail.com>
2023-10-14 12:12:30 -0700

    update

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+global.matchmakingServer = require('./matchmakingServer');
+
 
 app.use(cors());
 app.use(express.json());
@@ -25,4 +27,3 @@ app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
 
-global.matchmakingServer = new MatchMakingServer('dave', 1994);
